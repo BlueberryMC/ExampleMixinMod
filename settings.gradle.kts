a2dfcba852d3d9a9f163024c919c2878a128ad39
acrylic-style <me@acrylicstyle.xyz>
2021-12-05 16:20:29 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "ExampleMixinMod"
+
+pluginManagement {
+    repositories {
+        // mavenLocal()
+        mavenCentral()
+        maven { url = uri("https://repo2.acrylicstyle.xyz") }
+    }
+}
